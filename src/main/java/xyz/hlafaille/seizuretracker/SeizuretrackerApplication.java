0b8dfd388027bfc010c03e9d1f678453b08ec31f
hlafaille <hlafaille@proton.me>
2023-11-02 18:46:25 -0400

    Initial commit

---
+++ src/main/java/xyz/hlafaille/seizuretracker/SeizuretrackerApplication.java
@@ -0,0 +1,13 @@
+package xyz.hlafaille.seizuretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeizuretrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeizuretrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/hlafaille/seizuretracker/SeizuretrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hlafaille.seizuretracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeizuretrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
