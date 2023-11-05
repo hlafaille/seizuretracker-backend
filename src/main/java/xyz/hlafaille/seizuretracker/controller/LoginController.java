41506feb1505eeb4c179f2c600020122b95aee01
hlafaille <hlafaille@proton.me>
2023-11-05 18:22:15 -0500

    adding signup logic

--- src/main/java/xyz/hlafaille/seizuretracker/controller/LoginController.java
+++ src/main/java/xyz/hlafaille/seizuretracker/controller/LoginController.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
-    @GetMapping("/")
-    public String index(){
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
         return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
     }
 }
