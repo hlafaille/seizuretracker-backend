80ccdbb205d95b01066eefc9cd9c3bf340948fc9
hlafaille <hlafaille@proton.me>
2023-11-05 14:55:29 -0500

    got bootstrap and error handling going

---
+++ src/main/java/xyz/hlafaille/seizuretracker/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package xyz.hlafaille.seizuretracker.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        return "error";
+    }
+}
