38cb16e280acfe5a67f484a58351152d1086878c
hlafaille <hlafaille@proton.me>
2023-11-06 09:36:15 -0500

    added much improved error handling

--- src/main/java/xyz/hlafaille/seizuretracker/controller/ErrorController.java
+++ src/main/java/xyz/hlafaille/seizuretracker/controller/ErrorController.java
@@ -17,17 +17,21 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
         int statusCode = Integer.parseInt(statusCodeObj.toString());
 
         // begin defining our error messages
+        String funnyCardSubtitle = "Don't think that should happen...";
         String errorMessage = "Undefined error message";
         String detailedErrorMessage = "(no details on this error)";
         if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            funnyCardSubtitle = "This page is on vacation. Or just lost. Probably lost.";
             errorMessage = "Page not found";
         }
         else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-            errorMessage = "An internal server error has occurred";
+            funnyCardSubtitle = "Someone let the gremlins into our servers again. We're on it!";
+            errorMessage = "An internal server error has occurred. Please create a bug report.";
             detailedErrorMessage = throwable.getMessage();
         }
 
         // add these fields to the model, so they're accessible in the template
+        model.addAttribute("funnyCardSubtitle", funnyCardSubtitle);
         model.addAttribute("errorMessage", errorMessage);
         model.addAttribute("detailedErrorMessage", detailedErrorMessage);
         return "error";
