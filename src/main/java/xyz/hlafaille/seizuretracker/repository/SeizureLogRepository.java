d8b819b884d4d04f865fb638892b1e52f8d6caf5
hlafaille <hlafaille@proton.me>
2023-11-08 12:37:23 -0500

    #6 added repository annotation for all repositories

--- src/main/java/xyz/hlafaille/seizuretracker/repository/SeizureLogRepository.java
+++ src/main/java/xyz/hlafaille/seizuretracker/repository/SeizureLogRepository.java
@@ -2,10 +2,16 @@ package xyz.hlafaille.seizuretracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
 import xyz.hlafaille.seizuretracker.entity.SeizureLog;
+import xyz.hlafaille.seizuretracker.entity.User;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface SeizureLogRepository extends JpaRepository<SeizureLog, UUID>, QueryByExampleExecutor<SeizureLog> {
+    List<SeizureLog> findAllByUser(UUID userId);
 }
 
--- src/main/java/xyz/hlafaille/seizuretracker/repository/UserRepository.java
+++ src/main/java/xyz/hlafaille/seizuretracker/repository/UserRepository.java
@@ -3,12 +3,14 @@ package xyz.hlafaille.seizuretracker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
 import xyz.hlafaille.seizuretracker.entity.User;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID>, QueryByExampleExecutor<User> {
     Optional<User> findByEmail(String email);
 }
