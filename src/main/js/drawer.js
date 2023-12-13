f4b9d0e1b717f62c090d57c5be08eb0ceab722c4
Hunter <hlafaille@proton.me>
2023-12-13 11:37:41 -0500

    #22 made tailwind follow the same bundling format, updated naming convention for <head>

--- src/main/js/drawer.js
+++ src/main/js/drawer.js
@@ -3,7 +3,7 @@
  * Called from the hamburger in the navbar, toggles the drawer on and off
  */
 export function toggleDrawer() {
-    const element = document.getElementById("drawer");
+    const element = document.getElementById("navbarDrawer");
     if (element.classList.contains("hidden")) {
         element.classList.remove("hidden");
     } else {
