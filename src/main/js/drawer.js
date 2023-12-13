3254f2b0f5b2955bdb1ca7fb484aad6a881184fc
Hunter <hlafaille@proton.me>
2023-12-13 11:40:04 -0500

    #22 adding error message to toggleDrawer

--- src/main/js/drawer.js
+++ src/main/js/drawer.js
@@ -3,10 +3,14 @@
  * Called from the hamburger in the navbar, toggles the drawer on and off
  */
 export function toggleDrawer() {
-    const element = document.getElementById("navbarDrawer");
+    const element = document.getElementById("navbarDrawer")
+    if (!element) {
+        console.error("An element with id `navbarDrawer` does not exist")
+        return
+    }
     if (element.classList.contains("hidden")) {
-        element.classList.remove("hidden");
+        element.classList.remove("hidden")
     } else {
-        element.classList.add("hidden");
+        element.classList.add("hidden")
     }
 }
