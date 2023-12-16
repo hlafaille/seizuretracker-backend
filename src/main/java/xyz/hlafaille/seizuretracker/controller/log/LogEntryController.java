5ae341282d35952cb95c96551150b1a09e1b36a5
hlafaille <hlafaille@proton.me>
2023-12-16 01:12:03 -0500

    #22 basic multi part form works

--- src/main/java/xyz/hlafaille/seizuretracker/controller/log/LogEntryController.java
+++ src/main/java/xyz/hlafaille/seizuretracker/controller/log/LogEntryController.java
@@ -1,10 +1,19 @@
 package xyz.hlafaille.seizuretracker.controller.log;
 
 import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import xyz.hlafaille.seizuretracker.entity.Session;
+import xyz.hlafaille.seizuretracker.entity.User;
+import xyz.hlafaille.seizuretracker.exception.SessionCookieMissingException;
+import xyz.hlafaille.seizuretracker.exception.SessionEntityMissingException;
+import xyz.hlafaille.seizuretracker.exception.SessionUserMissingException;
+import xyz.hlafaille.seizuretracker.model.form.auth.CreateSeizureLogEntryFormModel;
 import xyz.hlafaille.seizuretracker.service.SeizureLogService;
 import xyz.hlafaille.seizuretracker.service.SessionService;
 
@@ -31,5 +40,31 @@ public class LogEntryController {
     @GetMapping("/logDashboard/newEntry/stepTwo")
     public String createLogEntryStepTwo(HttpServletRequest request, Model model) {
         return "fragments/log/logNewCards :: _stepTwoContent";
+    }
+
+    @GetMapping("/logDashboard/newEntry/stepThree")
+    public String createLogEntryStepThree(HttpServletRequest request, Model model) {
+        return "fragments/log/logNewCards :: _stepThreeContent";
+    }
+
+    @PostMapping("/logDashboard/newEntry")
+    public String doLogNewEntry(HttpServletRequest request, CreateSeizureLogEntryFormModel formData, Model model, HttpServletResponse response) throws SessionEntityMissingException, SessionUserMissingException, SessionCookieMissingException {
+        // get the user by their session id
+        Cookie sessionCookie = sessionService.getSessionCookieFromBrowserCookies(request.getCookies());
+        Session session = sessionService.getSessionEntityFromCookie(sessionCookie);
+        User user = sessionService.getUserEntityFromSessionId(session.getId());
+        seizureLogService.createLogEntry(
+                formData.getSeverity(),
+                user.getId(),
+                formData.getDuration(),
+                formData.getBeforeNote(),
+                formData.getDuringNote(),
+                formData.getAfterNote(),
+                formData.isHospitalVisitOccurred(),
+                formData.getAdditionalComment(),
+                false
+        );
+        response.addHeader("hx-redirect", "/logDashboard");
+        return "";
     }
 }
