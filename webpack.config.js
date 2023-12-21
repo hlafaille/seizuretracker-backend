c9405dec08273c8cb51f70be2101406aa0516008
hlafaille <hlafaille@proton.me>
2023-12-21 09:38:36 -0500

    #40 added overflow: hidden to the body tag

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/main/js/app.js',
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
             {
