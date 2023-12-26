58c95ddd13d064eb74d739353bf3e2985d7eb380
hlafaille <hlafaille@proton.me>
2023-12-26 13:34:24 -0500

    #69 adding mood survey controller

--- src/main/java/xyz/hlafaille/seizuretracker/dto/moodsurvey/CreateMoodSurveyEntryResponse.java
+++ src/main/java/xyz/hlafaille/seizuretracker/dto/moodsurvey/CreateMoodSurveyEntryResponse.java
@@ -1,4 +1,12 @@
 package xyz.hlafaille.seizuretracker.dto.moodsurvey;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
 public class CreateMoodSurveyEntryResponse {
+    private UUID moodSurveyEntryId;
 }
--- src/main/java/xyz/hlafaille/seizuretracker/restrcontroller/MoodSurveyController.java
+++ src/main/java/xyz/hlafaille/seizuretracker/restrcontroller/MoodSurveyController.java
@@ -1,12 +1,23 @@
 package xyz.hlafaille.seizuretracker.restrcontroller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.hlafaille.seizuretracker.dto.moodsurvey.CreateMoodSurveyEntryRequest;
 import xyz.hlafaille.seizuretracker.dto.moodsurvey.CreateMoodSurveyEntryResponse;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/moodSurvey")
 public class MoodSurveyController {
+    @PostMapping("")
+    public ResponseEntity<CreateMoodSurveyEntryResponse> createMoodSurveyEntry(@RequestBody CreateMoodSurveyEntryRequest requestBody) {
+        return ResponseEntity
+                .status(201)
+                .body(new CreateMoodSurveyEntryResponse(UUID.randomUUID()));
+    }
 }
